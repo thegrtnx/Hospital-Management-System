dae820752192aa1a043531b7c134c9d7499de941
Greatness Prodigy <49532326+thegrtnx@users.noreply.github.com>
2023-01-09 11:54:43 +0100

    dashboard completed

--- index.php
+++ index.php
@@ -119,14 +119,6 @@
     </div>
     <!-- / Layout wrapper -->
 
-    <div class="buy-now">
-      <a
-        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-        target="_blank"
-        class="btn btn-danger btn-buy-now"
-        >Upgrade to Pro</a
-      >
-    </div>
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
@@ -147,7 +139,6 @@
     <!-- Page JS -->
     <script src="assets/js/dashboards-analytics.js"></script>
 
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    
   </body>
 </html>
