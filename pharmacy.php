c320ddf9a00ac731d328ce9342aa8fcda4457d79
Greatness Prodigy <49532326+thegrtnx@users.noreply.github.com>
2023-05-02 19:49:19 +0100

    updated

--- pharmacy.php
+++ pharmacy.php
@@ -1,4 +1,50 @@
-<?php include("components/head.php") ?>
+<?php include("servl/protop.php") ?>
+<head>
+<meta charset="utf-8" />
+<meta
+  name="viewport"
+  content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+/>
+
+<title>Dashboard | Hospital Management System</title>
+
+<meta name="description" content="" />
+
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+  rel="stylesheet"
+/>
+
+<!-- Icons. Uncomment required icon fonts -->
+<link rel="stylesheet" href="assets/vendor/fonts/boxicons.css" />
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />
+<link rel="stylesheet" href="assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+<link rel="stylesheet" href="assets/css/demo.css" />
+
+<!-- Vendors CSS -->
+<link rel="stylesheet" href="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+<link rel="stylesheet" href="assets/vendor/libs/apex-charts/apex-charts.css" />
+<link rel="stylesheet" href="assets/css/bootstrap-4.min.css" />
+<link href="assets/css/toastr.css" rel="stylesheet"/>
+
+<!-- Page CSS -->
+
+<!-- Helpers -->
+<script src="assets/vendor/js/helpers.js"></script>
+
+<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+<script src="assets/js/config.js"></script>
+</head>
   <body>
     <!-- Layout wrapper -->
     <div class="layout-wrapper layout-content-navbar">
@@ -17,45 +63,71 @@
 
           <!-- / Navbar -->
 
-            <!-- Content wrapper -->
-            <div class="content-wrapper">
+           <!-- Content wrapper -->
+           <div class="content-wrapper">
             <!-- Content -->
 
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Upload Drugs for Pharmacy</h4>
 
-              <!-- Examples -->
-              <div class="row mb-5">
-                <div class="col-md-6 col-lg-4 mb-3">
+            <h4 class="fw-bold py-3 mb-4"><span class="text-dark">Upload Drug</h4>
 
-                
-                  <div class="card h-100">
-                    <img class="card-img-top" src="../assets/img/elements/2.jpg" alt="Card image cap" />
+              <div class="row">
+                <div class="col-md-12">
+                  
+                  <div class="card mb-4">
+                    <!-- Account -->
+                   
+                    <hr class="my-0" />
                     <div class="card-body">
-                      <h5 class="card-title">Ibupain</h5>
-                      
-                      <a href="javascript:void(0)" class="btn btn-outline-primary">Buy this drug</a>
+                      <form id="formAccountSettings" method="POST">
+                        <div class="row">
+                          <div class="mb-3 col-md-6">
+                            <label for="firstName" class="form-label">Drug Name</label>
+                            <input
+                              class="form-control"
+                              type="text"
+                              id="fname"
+                              name="firstName"
+                              value="<?php echo $t_users['fullname'] ?>"
+                              autofocus
+                            />
+                          </div>
+                        
+                          <div class="mb-3 col-md-6">
+                            <label for="email" class="form-label">Drug Price</label>
+                            <input
+                              class="form-control"
+                              type="email"
+                              id="email"
+                              name="email"
+                              value="<?php echo $_SESSION['login'] ?>" />
+                          </div>
+                         
+                                               
+
+
+                        </div>
+                        <div class="mt-2">
+                          <button type="button" class="btn btn-primary me-2" id="updtpro">Save changes</button>
+                        </div>
+                      </form>
                     </div>
+                    <!-- /Account -->
                   </div>
-
-
+                 
                 </div>
-               
               </div>
-              <!-- Examples -->
-
-
             </div>
             <!-- / Content -->
 
             <!-- Footer -->
-            <<?php include("components/footer.php") ?>
+            <?php include("components/footer.php") ?>
             <!-- / Footer -->
 
             <div class="content-backdrop fade"></div>
           </div>
           <!-- Content wrapper -->
-
+          
         </div>
         <!-- / Layout page -->
       </div>
